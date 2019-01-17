01eda3bbb424aaf3bb274f63b18dc9a2c42d2440
Spencer McIntyre <zeroSteiner@gmail.com>
2019-01-17 12:05:43 -0500

    Add a couple new IR components for convenience

--- crimson_forge/ir.py
+++ crimson_forge/ir.py
@@ -46,7 +46,39 @@ class JumpKind(pyvex.lifting.util.JumpKind):
 		return value in (cls.Call, cls.Syscall, cls.Sysenter)
 
 # hashable, immutable
+class IRJump(object):
+	__slots__ = ('_arch', '_from_address', '_to_address', '_kind')
+	def __init__(self, arch, to_address, from_address, kind):
+		self._arch = arch
+		self._to_address = to_address
+		self._from_address = from_address
+		self._kind = kind
+
+	def __hash__(self):
+		return hash(((self._arch.name, self._arch.bits, self._arch.memory_endness), self._to_address, self._from_address))
+
+	def __repr__(self):
+		return "<{} 0x{:04x} -> 0x{:04x} ({}) >".format(self.__class__.__name__, self._from_address, self._to_address, self._kind)
+
+	@property
+	def arch(self):
+		return self._arch
+
+	@property
+	def to_address(self):
+		return self._to_address
+
+	@property
+	def from_address(self):
+		return self._from_address
+
+	@property
+	def kind(self):
+		return self._kind
+
+# hashable, immutable
 class IRRegister(object):
+	__slots__ = ('_arch', '_positions')
 	def __init__(self, arch, positions):
 		self._arch = arch
 		self._positions = positions
@@ -61,7 +93,7 @@ class IRRegister(object):
 		return self._positions == other._positions
 
 	def __hash__(self):
-		return hash((self._positions, (self._arch.name, self._arch.bits, self._arch.memory_endness)))
+		return hash(((self._arch.name, self._arch.bits, self._arch.memory_endness), self._positions))
 
 	def __repr__(self):
 		return "<{0} name: {1!r} width: {2} >".format(self.__class__.__name__, self.name, self.width)
@@ -123,6 +155,23 @@ class IRRegister(object):
 
 def lift(blob, base, arch):
 	return pyvex.lift(blob, base, arch, opt_level=OPT_LEVEL_NO_OPTIMIZATION)
+
+def irsb_address_for_statement(irsb, stmt):
+	"""
+	Take a lifted *irsb* object and return the address of the instruction to
+	which *stmt* belongs.
+
+	:param irsb: The IR super-block to which *stmt* belongs.
+	:param stmt: The IR statement to get the instruction address for.
+	:rtype: int
+	"""
+	address = None
+	for o_stmt in irsb.statements:
+		if isinstance(o_stmt, pyvex.stmt.IMark):
+			address = o_stmt.addr
+		if o_stmt is stmt:
+			return address
+	return None
 
 def irsb_to_instructions(irsb):
 	"""
