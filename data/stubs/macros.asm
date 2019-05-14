5fc4ba67bf2774a13e9ec834cfcd599bc55938e9
Spencer McIntyre <zeroSteiner@gmail.com>
2019-05-14 14:44:52 -0400

    Fix a string counting bug in macros

--- data/stubs/macros.asm
+++ data/stubs/macros.asm
@@ -1,12 +1,12 @@
-{% set ___macro_strings = 0 %}
+{% set ___macro_strings = [] %}
 
 {% macro load_string(value, register) -%}
-  {% set _macro_strings = ___macro_strings + 1 -%}
+  {% do ___macro_strings.append(value) -%}
   ; {{ register }} = &'{{ value }}'
-  call _macro_string{{ ___macro_strings }}
+  call _macro_string{{ ___macro_strings | length }}
   {% for source_line in raw_string(value) %}
   {{ source_line.code }}
   {% endfor %}
-_macro_string{{ ___macro_strings }}:
+_macro_string{{ ___macro_strings | length }}:
   pop {{ register }}
 {%- endmacro %}
